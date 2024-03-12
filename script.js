74a03934ed93f5c3facd3852f44074a8667e7932
shahzad akram <alyakram786@gmail.com>
2024-03-12 22:03:09 +0500

    making pop manu dis

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const inputbox = document.getElementById("input-box");
 const listCont = document.getElementById("list-Container");
+let popup = document.querySelector(".pop");
+let p = document.getElementById("p");
+let iR = document.getElementById("iR");
+let iG = document.getElementById("iG");
+let line = document.getElementById("line");
 
 function addTask() {
+
+
+    setTimeout(function() {
+        popup.style.display = "none";
+    },2000)
+
+
   if (inputbox.value === "") {
     console.log("if its empty");
+    iR.style.display = "block";
+    iG.style.display = "none";
+    line.style.background = "red";
+    p.innerText = "Please provide a value";
+    popup.style.display = "block";
 
     alert("Please enter a task");
 
@@ -23,6 +40,8 @@ function addTask() {
 
 listCont.addEventListener("click" , function(e){
 
+
+    
 
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");;
